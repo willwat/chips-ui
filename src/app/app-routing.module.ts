092c401bc19b38d99a2724dae09d0f277c6c4395
willwat <willwatson220@gmail.com>
2023-08-05 01:27:24 -0400

    Implemented other pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TrimmingComponent } from './trimming/trimming.component';
+import { RemovalComponent } from './removal/removal.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'trimming', component: TrimmingComponent },
+  { path: 'removal', component: RemovalComponent },
+  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
